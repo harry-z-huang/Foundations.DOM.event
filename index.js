a396e53fb847e0207cae213811c0455315651201
RWalk41 <rwalk41@gmail.com>
2022-08-24 15:04:45 -0500

    finished mag

--- index.js
+++ index.js
@@ -14,25 +14,30 @@
 // });
 
 const mag = document.querySelector("#magnifying-glass");
-const button1 = document.querySelector("");
-const button2 = document.querySelector("");
+const button1 = document.querySelector("#Button1");
+const button2 = document.querySelector("#Button2");
 const Words = document.createElement("p")
 
 
 Words.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sagittis id consectetur purus ut. Nunc mattis enim ut tellus elementum sagittis. Tincidunt ornare massa eget egestas purus viverra accumsan in. Proin sagittis nisl rhoncus mattis rhoncus urna neque. Quisque egestas diam in arcu cursus euismod. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam purus. Sapien et ligula ullamcorper malesuada proin. Nec feugiat nisl pretium fusce id. Diam sollicitudin tempor id eu nisl nunc mi ipsum. Lorem ipsum dolor sit amet consectetur adipiscing elit. Sed id semper risus in hendrerit. Diam quis enim lobortis scelerisque. Tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum. Nunc lobortis mattis aliquam faucibus purus. Enim lobortis scelerisque fermentum dui faucibus in ornare quam viverra."
+Size = 20;
+Words.style.fontSize = Size.toString() + "px";
 
-fontSize =
 button1.addEventListener("click", function () {
-  fontSize++;
-  Words.fontSize=fontSize;
-  
+  Size++;
+  Words.style.fontSize = Size.toString() + "px";
+
 
 });
 
 button2.addEventListener("click", function () {
-  fontSize--;
+  Size--;
+  Words.style.fontSize = Size.toString() + "px";
 
 
 });
 
+
+mag.appendChild(button1);
+mag.appendChild(button2);
 mag.appendChild(Words);
