f26840f092f4008c9226a8f20075dacb43e395d2
RWalk41 <rwalk41@gmail.com>
2022-08-24 14:31:01 -0500

    re-commiting

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ const button2 = document.querySelector("");
 const Words = document.createElement("p")
 
 
-Words.textContent =Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sagittis id consectetur purus ut. Nunc mattis enim ut tellus elementum sagittis. Tincidunt ornare massa eget egestas purus viverra accumsan in. Proin sagittis nisl rhoncus mattis rhoncus urna neque. Quisque egestas diam in arcu cursus euismod. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam purus. Sapien et ligula ullamcorper malesuada proin. Nec feugiat nisl pretium fusce id. Diam sollicitudin tempor id eu nisl nunc mi ipsum. Lorem ipsum dolor sit amet consectetur adipiscing elit. Sed id semper risus in hendrerit. Diam quis enim lobortis scelerisque. Tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum. Nunc lobortis mattis aliquam faucibus purus. Enim lobortis scelerisque fermentum dui faucibus in ornare quam viverra.
+Words.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sagittis id consectetur purus ut. Nunc mattis enim ut tellus elementum sagittis. Tincidunt ornare massa eget egestas purus viverra accumsan in. Proin sagittis nisl rhoncus mattis rhoncus urna neque. Quisque egestas diam in arcu cursus euismod. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam purus. Sapien et ligula ullamcorper malesuada proin. Nec feugiat nisl pretium fusce id. Diam sollicitudin tempor id eu nisl nunc mi ipsum. Lorem ipsum dolor sit amet consectetur adipiscing elit. Sed id semper risus in hendrerit. Diam quis enim lobortis scelerisque. Tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum. Nunc lobortis mattis aliquam faucibus purus. Enim lobortis scelerisque fermentum dui faucibus in ornare quam viverra."
 
-fontSize = 10px;
+fontSize =
 button1.addEventListener("click", function () {
   fontSize++;
   Words.fontSize=fontSize;
